5f0baf854993ede86a0902f84ee614e16f298f7b
dc <3101348575@qq.com>
2020-09-20 21:48:26 +0800

    Initial commit

---
+++ spring-mvc/src/main/java/com/easy/archiecture/config/CacheConfig.java
@@ -0,0 +1,31 @@
+package com.easy.archiecture.config;
+
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.config.CacheManagementConfigUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager.create(redisConnectionFactory);
+    }
+
+
+}
